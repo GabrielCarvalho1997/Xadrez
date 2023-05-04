989293c73a6a7afcc24bb2ec3eacc50a27f65f76
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2023-05-04 15:47:50 -0300

    Piece move count

--- src/application/UI.java
+++ src/application/UI.java
@@ -67,7 +67,6 @@ public class UI {
 			System.out.println("CHECKMATE!");
 			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
-
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
